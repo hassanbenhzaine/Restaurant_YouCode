8a6f84d6853bf8e30340b6cdd78056f6d91d1aee
Hassan Benhzaine <hassanbenhzaine@users.noreply.github.com>
2021-04-10 20:40:17 +0100

    Update logout.php

--- Source code/logout.php
+++ Source code/logout.php
@@ -3,7 +3,7 @@
 if(isset($_POST["logout"])){
     session_unset();
     session_destroy();
-    setcookie("stay","1",time(),"/","localhost",0,1);
+    setcookie("stay","1",time());
     header("location:index.php");
 } else{
     header("location:index.php");
